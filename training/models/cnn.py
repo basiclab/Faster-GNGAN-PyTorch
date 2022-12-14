2db787360ea263375d688be4ebe54af70ec269e9
YiLun Wu <w86763777@gmail.com>
2022-12-14 08:33:55 +0000

    rescale cnn

--- training/models/cnn.py
+++ training/models/cnn.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 
 
-class DiscriminatorK(nn.Module):
+from training.models.base import RescalableSequentialModel, RescalableWrapper
+
+
+class DiscriminatorK(RescalableSequentialModel):
     def __init__(self, resolution, n_classes, num_per_block=1):
         super().__init__()
         config = {
@@ -16,22 +19,22 @@ class DiscriminatorK(nn.Module):
         now_ch = 3
         for ch in [64, 128, 256]:
             for _ in range(num_per_block - 1):
-                blocks.append(nn.Conv2d(
-                    now_ch, ch, kernel_size=3, stride=1, padding=1))
+                blocks.append(RescalableWrapper(nn.Conv2d(
+                    now_ch, ch, kernel_size=3, stride=1, padding=1)))
                 blocks.append(nn.LeakyReLU(0.1, inplace=True))
                 now_ch = ch
-            blocks.append(nn.Conv2d(
-                now_ch, ch * 2, kernel_size=4, stride=2, padding=1))
+            blocks.append(RescalableWrapper(nn.Conv2d(
+                now_ch, ch * 2, kernel_size=4, stride=2, padding=1)))
             blocks.append(nn.LeakyReLU(0.1, inplace=True))
             now_ch = ch * 2
         blocks.append(nn.Flatten(start_dim=1))
-        blocks.append(nn.Linear(M * M * 512, 1))
+        blocks.append(RescalableWrapper(nn.Linear(M * M * 512, 1)))
         self.main = nn.Sequential(*blocks)
 
         for m in self.modules():
-            if isinstance(m, (nn.Conv2d, nn.Linear)):
-                torch.nn.init.normal_(m.weight, std=0.02)
-                torch.nn.init.zeros_(m.bias)
+            if isinstance(m, RescalableWrapper):
+                torch.nn.init.normal_(m.module.weight, std=0.02)
+                torch.nn.init.zeros_(m.module.bias)
 
     def forward(self, x, *args, **kwargs):
         y = self.main(x)
